83b9697d595a1f8788a21f6aaa56a5fe27d3d702
SUNNY KATARIYA <53706733+skpycoder786@users.noreply.github.com>
2021-08-10 23:45:57 +0530

    Add files via upload
    Basic form completed

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+var card = document.getElementById("card");
+
+function openRegister(){
+  card.style.transform ="rotateY(-180deg)";
+}
+function openLogin(){
+  card.style.transform ="rotateY(-360deg)";
+}
